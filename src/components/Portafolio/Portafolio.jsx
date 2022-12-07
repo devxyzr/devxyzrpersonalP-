2f175dc5dd5ba796b553ec6006677277bf60621a
devxyzr <develop.xyzr@gmail.com>
2022-12-07 16:23:04 -0500

    Add section portfolio and working in testimonials

--- src/components/Portafolio/Portafolio.jsx
+++ src/components/Portafolio/Portafolio.jsx
@@ -1,8 +1,80 @@
 import React from 'react';
 import './portafolio.css';
+import IMGWORK from '../../assets/Octocat.png';
 
 const Portafolio = () => {
-  return <section id="portafolio">Portafolio</section>;
+  const demoWork = [
+    {
+      id: 1,
+      image: IMGWORK,
+      title: 'Title to work',
+      github: 'https://github.com/devxyzr',
+      demo: 'https://github.com/devxyzr',
+    },
+    {
+      id: 2,
+      image: IMGWORK,
+      title: 'Title to work',
+      github: 'https://github.com/devxyzr',
+      demo: 'https://github.com/devxyzr',
+    },
+    {
+      id: 3,
+      image: IMGWORK,
+      title: 'Title to work',
+      github: 'https://github.com/devxyzr',
+      demo: 'https://github.com/devxyzr',
+    },
+    {
+      id: 4,
+      image: IMGWORK,
+      title: 'Title to work',
+      github: 'https://github.com/devxyzr',
+      demo: 'https://github.com/devxyzr',
+    },
+    {
+      id: 5,
+      image: IMGWORK,
+      title: 'Title to work',
+      github: 'https://github.com/devxyzr',
+      demo: 'https://github.com/devxyzr',
+    },
+    {
+      id: 6,
+      image: IMGWORK,
+      title: 'Title to work',
+      github: 'https://github.com/devxyzr',
+      demo: 'https://github.com/devxyzr',
+    },
+  ];
+
+  return (
+    <section id="portafolio">
+      <h5>My Rencent Work</h5>
+      <h2>Portafolio </h2>
+
+      <div className="container portafolio_container">
+        {demoWork.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portafolio_item">
+              <div className="portafolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portafolio_item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portafolio;
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import './testimonials.css';
+import IMG from '../../assets/Octocat.png';
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials </section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={IMG} alt="avatar one" />
+          </div>
+          <h5 className="client_name">Ernest Achiever</h5>
+          <small className="client_review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            consequuntur suscipit tenetur obcaecati accusamus enim, possimus,
+            aperiam veniam, perspiciatis beatae commodi inventore veritatis vero
+            aspernatur amet doloremque similique sunt qui.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={IMG} alt="avatar one" />
+          </div>
+          <h5 className="client_name">Ernest Achiever</h5>
+          <small className="client_review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            consequuntur suscipit tenetur obcaecati accusamus enim, possimus,
+            aperiam veniam, perspiciatis beatae commodi inventore veritatis vero
+            aspernatur amet doloremque similique sunt qui.
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client_avatar">
+            <img src={IMG} alt="avatar one" />
+          </div>
+          <h5 className="client_name">Ernest Achiever</h5>
+          <small className="client_review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            consequuntur suscipit tenetur obcaecati accusamus enim, possimus,
+            aperiam veniam, perspiciatis beatae commodi inventore veritatis vero
+            aspernatur amet doloremque similique sunt qui.
+          </small>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
