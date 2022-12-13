bb27476e61852924559517941d1d5fb3bc5d3aea
devxyzr <develop.xyzr@gmail.com>
2022-12-12 22:15:00 -0500

    Add a component

--- src/components/Contacts/Contact.jsx
+++ src/components/Contacts/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './contact.css';
+import { MdOutlineEmail } from 'react-icons/md';
 
 const Contact = () => {
-  return <section id="contact">Contacts</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me </h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>develop.xyzr@gmail.com</h5>
+            <a href="mailto:develop.xyzr@gmail.com">Send a message</a>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,50 +2,61 @@ import React from 'react';
 import './testimonials.css';
 import IMG from '../../assets/Octocat.png';
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const Testimonials = () => {
+  const testimonials = [
+    {
+      avatar: IMG,
+      name: 'Toallin SouthPark',
+      review:
+        ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis non distinctio laboriosam veritatis ipsam eos adipisci hic sequi, accusantium fuga dignissimos atque porro placeat officia ipsa ipsum similique culpa quas.',
+    },
+    {
+      avatar: IMG,
+      name: 'Toallin SouthPark',
+      review:
+        ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis non distinctio laboriosam veritatis ipsam eos adipisci hic sequi, accusantium fuga dignissimos atque porro placeat officia ipsa ipsum similique culpa quas.',
+    },
+    {
+      avatar: IMG,
+      name: 'Toallin SouthPark',
+      review:
+        ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis non distinctio laboriosam veritatis ipsam eos adipisci hic sequi, accusantium fuga dignissimos atque porro placeat officia ipsa ipsum similique culpa quas.',
+    },
+  ];
+
   return (
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials_container">
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={IMG} alt="avatar one" />
-          </div>
-          <h5 className="client_name">Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-            consequuntur suscipit tenetur obcaecati accusamus enim, possimus,
-            aperiam veniam, perspiciatis beatae commodi inventore veritatis vero
-            aspernatur amet doloremque similique sunt qui.
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={IMG} alt="avatar one" />
-          </div>
-          <h5 className="client_name">Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-            consequuntur suscipit tenetur obcaecati accusamus enim, possimus,
-            aperiam veniam, perspiciatis beatae commodi inventore veritatis vero
-            aspernatur amet doloremque similique sunt qui.
-          </small>
-        </article>
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={IMG} alt="avatar one" />
-          </div>
-          <h5 className="client_name">Ernest Achiever</h5>
-          <small className="client_review">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-            consequuntur suscipit tenetur obcaecati accusamus enim, possimus,
-            aperiam veniam, perspiciatis beatae commodi inventore veritatis vero
-            aspernatur amet doloremque similique sunt qui.
-          </small>
-        </article>
-      </div>
+      <Swiper
+        className="container testimonials_container"
+        smodules={[Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {testimonials.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={avatar} alt="avatar one" />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
